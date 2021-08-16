94889d3a9cd5e19d0d87fff482be9512e16d91fd
EC2 Default User <ec2-user@ip-172-31-23-113.ec2.internal>
2021-08-16 15:26:14 +0000

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_04_091351) do
+ActiveRecord::Schema.define(version: 2021_08_16_152352) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "end_user_id", null: false
