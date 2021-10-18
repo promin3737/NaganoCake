9816a7bcedac017b7d2b42df48173daab7615c9c
promin3737 <s.s.hayabusa3737@icloud.com>
2021-10-18 01:35:20 +0000

    README 更新

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -43,6 +43,7 @@ class Public::OrdersController < ApplicationController
   end
 
   def complete
+    @rank_items = Item.find(Favorite.group(:item_id).order('count(item_id) desc').limit(3).pluck(:item_id))
   end
 
   def confirm
