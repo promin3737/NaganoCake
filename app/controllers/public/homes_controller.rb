625d7dd4c30eb4a28b4645d0e0bda46e6bdb2b8f
promin3737 <s.s.hayabusa3737@icloud.com>
2021-12-13 07:35:50 +0000

    [Finish] 注文完了ページ デザイン

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,7 @@
 class Public::HomesController < ApplicationController
   def top
     # @items = Item.all
-    @rank_items = Item.find(Favorite.group(:item_id).order('count(item_id) desc').limit(3).pluck(:item_id))
+    @rank_items = Item.find(Favorite.group(:item_id).order('count(item_id) desc').limit(3).pluck(:item_id))#まず、記事の番号(note_id)が同じものにグループを分ける。それを、番号の多い順に並び替え、表示する最大数を3個に指定する。そして最後に:note_idカラムのみを数字で取り出すように指定。
   end
 
   def about
